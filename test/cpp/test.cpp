1d108522c7f1da24f292133110f9399c49243912
Jan <jandrole2@gmail.com>
2023-06-19 13:12:37 +0200

    added iptables rule test

--- test/cpp/test.cpp
+++ test/cpp/test.cpp
@@ -36,30 +36,59 @@ std::string executeCommand(const std::string& command) {
 
 
 std::string ipAddresses[] = {
-        "46.122.11.22",
-        "46.122.11.23",
-        "46.122.11.24",
-        "46.122.11.25",
-        "46.122.11.26",
-        "46.122.11.27"
+        "10.0.1.78"
+        //"46.122.11.22",
+        //"46.122.11.23",
+        //"46.122.11.24",
+        //"46.122.11.25",
+        //"46.122.11.26",
+        //"46.122.11.27"
     };
 
 
+/*
 TEST_CASE("Testing Ping"){
     for (const auto& ip : ipAddresses) {
         std::string command = std::string("bash ../../src/pingTest.sh ").append(ip);
         INFO("Device ip :",ip);
         CHECK(executeCommand(command) == "successful");
     }
-    
 }
-
 TEST_CASE("Testing SSH"){
     for (const auto& ip : ipAddresses) {
         std::string command = std::string("bash ../../src/sshTest.sh ").append(ip);
         INFO("Device ip :",ip);
+        CHECK(executeCommand(command) == "successful");      
+    }
+}
+
+TEST_CASE("Testing if ip rule is up"){
+    for (const auto& ip : ipAddresses) {
+        std::string command = std::string("bash ../../src/iptablesRuleTest.sh ").append(ip);
+        INFO("Device ip :",ip);
         CHECK(executeCommand(command) == "successful");
     }
-    
 }
+*/
+
+TEST_CASE("Testing devices"){
+    for (const auto& ip : ipAddresses) {
+        INFO("Device ip :",ip);
+
+        SUBCASE("Testing Ping"){
+            std::string command = std::string("bash ../../src/pingTest.sh ").append(ip);
+            CHECK(executeCommand(command) == "successful");
+        }
+        SUBCASE("Testing SSH"){
+            std::string command = std::string("bash ../../src/sshTest.sh ").append(ip);
+            CHECK(executeCommand(command) == "successful"); 
+        }
+        SUBCASE("Testing if ip rule is up"){
+            std::string command = std::string("bash ../../src/iptablesRuleTest.sh ").append(ip);
+            CHECK(executeCommand(command) == "successful"); 
+        }
+    }
+}
+
+
 
